c58f6d0ff23e093255d419d2b1f78269d1d0f693
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-22 23:25:37 +0530

    Firebase connection

--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
+//Colors
 const backgroundColor = Color.fromARGB(255, 4, 31, 53);
 var buttonColor = Color.fromARGB(255, 242, 249, 255);
+
+//Firebase
+var firebaseAuth = FirebaseAuth.instance;
+var firebaseStorage = FirebaseStorage.instance;
---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:videoapp/const.dart';
+
+class AuthController extends GetxController {
+  //Uploading Picked Image to storage
+  Future<String> _uploadImageStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  Future<void> registerUser(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        String downloadUrl = await _uploadImageStorage(image);
+      }
+    } catch (err) {
+      Get.snackbar("Error creating account", err.toString());
+    }
+  }
+}
