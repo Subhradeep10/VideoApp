55ec66f9067f7a3f2bfb3aed34a92726964c69a9
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-22 21:41:11 +0530

    Login Screen build

---
+++ lib/const.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromARGB(255, 4, 31, 53);
--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -6,8 +6,31 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('LoginScreen'),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                hintText: 'Email',
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                hintText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
