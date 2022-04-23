073beb511b03ee18a2dcbaadc0fe03957daded93
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-23 07:37:52 +0530

    Authentication done

--- lib/const.dart
+++ lib/const.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -9,3 +10,4 @@ var buttonColor = Color.fromARGB(255, 242, 249, 255);
 //Firebase
 var firebaseAuth = FirebaseAuth.instance;
 var firebaseStorage = FirebaseStorage.instance;
+var firestore = FirebaseFirestore.instance;
