bc14d8c192567736954961b044090ba9fe2b2cca
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-23 07:40:44 +0530

    User Model is created

---
+++ lib/model/user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class User {
+  String name;
+  String profilePhoto;
+  String email;
+  String uid;
+
+  User({
+    required this.name,
+    required this.profilePhoto,
+    required this.email,
+    required this.uid,
+  })
+}
